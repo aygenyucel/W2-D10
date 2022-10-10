b8102f457f8597150b3b733b0ecae0d871bd4de3
Aygen Yucel <aygenyucel@outlook.com>
2022-10-10 11:04:23 +0300

    exercise 1 to 6 done in Functions.js

--- d9-functions/Functions.js
+++ d9-functions/Functions.js
@@ -1,45 +1,75 @@
-/*
-ASSIGNMENT RULES
-- All the answers must be written in JavaScript
-- You can ask for help, reach the Teaching Assistants if needed
-- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
-- You can test your code in a separate file or de-commenting the single exercises in this one.
-- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
-- The solution must be pushed to the repository and be available for the tutors by the end of the day (5PM CET)
-*/
-
 /* EXERCISE 1
  Write a function called "area" which receives 2 parameters (l1, l2) and calculates the area of the associated rectangle.
 */
+console.log("\n-------------------EXERCISE 1--------------------");
 
-/* WRITE YOUR ANSWER HERE */
+function area(l1, l2) {
+  return 2 * (l1 + l2);
+}
+console.log("area of rectangle(5,10):", area(5, 10));
 
 /* EXERCISE 2
  Write a function called "crazySum" which receives two integers as parameters.
  It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n-------------------EXERCISE 2--------------------");
+
+function crazySum(int1, int2) {
+  let sum = int1 + int2;
+  if (int1 === int2) {
+    return sum * 3;
+  }
+  return sum;
+}
+
+console.log("crazySum(6, 6) --->", crazySum(6, 6));
 
 /* EXERCISE 3
  Write a function called "crazyDiff" that computes the absolute difference between a given number and 19.
  It should return triple their absolute difference if the given number is greater than 19.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n-------------------EXERCISE 3--------------------");
+
+function crazyDiff(num) {
+  let difference = Math.abs(num - 19);
+  let triple = Math.pow(difference, 3);
+  return triple;
+}
+
+console.log("crazDiff(16):", crazyDiff(16));
 
 /* EXERCISE 4
  Write a function called "boundary" which accept an integer parameter n and returns true if n is within 20 and 100 (included) or if n it's equal to 400.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n-------------------EXERCISE 4--------------------");
+
+function boundary(n) {
+  if ((n >= 20 && n <= 100) || n === 400) {
+    return true;
+  }
+  return false;
+}
+
+console.log("boundary(45):", boundary(45));
 
 /* EXERCISE 5
  Write a function called "strivify" which accepts a string as a parameter.
  It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n-------------------EXERCISE 5--------------------");
+
+function strivify(str) {
+  strArr = str.split(" ");
+  strArr.unshift("Strive");
+  newStr = strArr.join(" ");
+  return newStr;
+}
+
+console.log(strivify("I love Epicode"));
 
 /* EXERCISE 6
  Write a function called "check3and7" which accepts a positive number as a parameter and checks if it is a multiple of 3 or a multiple of 7.
